447d1f35303983e41ea60cff71c83e4f3f6d3b0d
eliasmpw <eliasmpw@hotmail.com>
2022-03-26 18:17:51 -0400

    Add client with base web3 connection

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -33,6 +35,8 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+
+   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -84,10 +88,10 @@ module.exports = {
       version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
       //  evmVersion: "byzantium"
       // }
     }
